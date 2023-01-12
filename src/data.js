e1bc1607fd9a64bf957ede4c7e11ab33f9ed6797
luk-fontain <luciano_rios@gmx.de>
2023-01-12 10:02:48 +0100

    aktuell-12-01

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "Technical page",
+    subtitle: "HTML and CSS",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.png",
+    link: "https://codepen.io/L-R-M/pen/ZELjaGw",
+  },
+  {
+    title: "Himmelstreppe",
+    subtitle: "HTML and CSS",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.png",
+    link: "https://codepen.io/L-R-M/pen/jOwwPba",
+  },
+  {
+    title: "Potfolio I",
+    subtitle: "HTML and CSS",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.png",
+    link: "https://codepen.io/L-R-M/pen/jOyXPPj",
+  },
+  {
+    title: "App",
+    subtitle: "React",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.png",
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Cat ipsum dolor sit amet, ignore the squirrels, you’ll never catch them anyway. Rub face on owner my left donut is missing, as is my right please let me outside pouty face yay! wait, it’s cold out please let me inside pouty face oh, thank you rub against mommy’s leg oh it looks so nice out, please let me outside again the neighbor cat was mean to me please let me back inside destroy house in 5 seconds. Sleep on keyboard i’m so hungry i’m so hungry but ew not for that scratch leg; meow for can opener to feed me present belly, scratch hand when stroked play riveting piece on synthesizer keyboard. Burrow under covers..",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse James",
+    company: "DCI",
+  },
+  {
+    quote:
+      "Cat ipsum dolor sit amet, ignore the squirrels, you’ll never catch them anyway. Rub face on owner my left donut is missing, as is my right please let me outside pouty face yay! wait, it’s cold out please let me inside pouty face oh, thank you rub against mommy’s leg oh it looks so nice out, please let me outside again the neighbor cat was mean to me please let me back inside destroy house in 5 seconds. Sleep on keyboard i’m so hungry i’m so hungry but ew not for that scratch leg; meow for can opener to feed me present belly, scratch hand when stroked play riveting piece on synthesizer keyboard. Burrow under covers..",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Lionel Messi",
+    company: "Seleccion Argentina",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "Sass",
+  "Git",
+  "Bootstrap",
+  "JavaScript",
+  "React",
+  "Node.js",
+];
