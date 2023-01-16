016a85e5b9678ebb48e7707cf7ca42395a0bb1f8
luk-fontain <luciano_rios@gmx.de>
2023-01-16 11:34:45 +0100

    test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Testimonials from "./components/Testimonials";
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
+    <h1>CIao!</h1>
       <Navbar />
       <About />
       <Projects />
